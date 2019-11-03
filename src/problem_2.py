8c7044940c531068bfd980c856ee9d8d5cd3838b
sebastian <sblanco@devartis.com>
2019-11-03 16:42:49 -0300

    change probabilities when no request are in the server

--- src/problem_2.py
+++ src/problem_2.py
@@ -3,31 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-class Server:
-    def __init__(self, total_time=1000000, time_between_arrivals=10):
-        """
-        total_time: total time in milli-seconds
-        time_between_arrivals: time for arrival or processing completion
-        in milli-seconds
-        p: arrival_prob
-        q: request_completion_prob
-        """
-        self.total_time = total_time
-        self.time_between_arrivals = time_between_arrivals
-        self.p = 1/40
-        self.q = 1/30
-        self.request_number = 0
-        self.change_state = {
-            'move_forward_prob': self.add_request,
-            'go_back_prob': self.finish_request,
-            'staying_prob': lambda: None
-        }
-        self.occurrences_per_time = {}
-        self.occurrences_per_state = {}
+class Probabilities:
+    def __init__(self, request_number):
+        self.p = 1 / 40
+        self.q = 1 / 30
+        self.request_number = request_number
         self.probs = [
             {
-               'prob': self.move_forward_prob(),
-               'name': 'move_forward_prob'
+                'prob': self.move_forward_prob(),
+                'name': 'move_forward_prob'
             },
             {
                 'prob': self.go_back_prob(),
@@ -39,10 +23,19 @@ class Server:
             }
         ]
         self.probs = sorted(self.probs, key=lambda x: x['prob'])
-        for idx in range(len(self.probs)-1):
-            self.probs[idx+1]['prob'] += self.probs[idx]['prob']
+        for idx in range(len(self.probs) - 1):
+            self.probs[idx + 1]['prob'] += self.probs[idx]['prob']
+        self.change_state = {
+            'move_forward_prob': self.add_request,
+            'go_back_prob': self.finish_request,
+            'staying_prob': lambda: None
+        }
 
-        self.no_processing_request_number = 0
+    def next(self, action):
+        self.change_state[action]()
+        if self.request_number == 0:
+            return ZeroRequestProbabilities(self.request_number)
+        return GreaterZeroRequestProbabilities(self.request_number)
 
     def add_request(self):
         self.request_number += 1
@@ -53,15 +46,52 @@ class Server:
         self.request_number -= 1
 
     def staying_prob(self):
-        p = self.p
-        q = self.q
-        return p*q + (1-p)*(1-q)
+        raise Exception('not implemented')
 
     def move_forward_prob(self):
-        return self.p*(1-self.q)
+        raise Exception('not implemented')
 
     def go_back_prob(self):
-        return self.q*(1-self.p)
+        raise Exception('not implemented')
+
+
+class ZeroRequestProbabilities(Probabilities):
+    def staying_prob(self):
+        return 1 - self.p
+
+    def move_forward_prob(self):
+        return self.p
+
+    def go_back_prob(self):
+        return 0
+
+
+class GreaterZeroRequestProbabilities(Probabilities):
+    def staying_prob(self):
+        p = self.p
+        q = self.q
+        return p * q + (1 - p) * (1 - q)
+
+    def move_forward_prob(self):
+        return self.p * (1 - self.q)
+
+    def go_back_prob(self):
+        return self.q * (1 - self.p)
+
+
+class Server:
+    def __init__(self, total_time=1000000, time_between_arrivals=10):
+        """
+        total_time: total time in milli-seconds
+        time_between_arrivals: time for arrival or processing completion
+        in milli-seconds
+        """
+        self.total_time = total_time
+        self.time_between_arrivals = time_between_arrivals
+        self.occurrences_per_time = {}
+        self.occurrences_per_state = {}
+        self.no_processing_request_number = 0
+        self.probabilities = ZeroRequestProbabilities(0)
 
     def graph_occurrences_per_state(self):
         x = list(self.occurrences_per_state.keys())
@@ -84,23 +114,26 @@ class Server:
         return self.no_processing_request_number * \
                self.time_between_arrivals/self.total_time*100
 
+    def log_stats(self, timestamp):
+        request_number = self.probabilities.request_number
+        self.occurrences_per_time.setdefault(timestamp, 0)
+        self.occurrences_per_time[timestamp] += request_number
+        self.occurrences_per_state.setdefault(request_number, 0)
+        self.occurrences_per_state[request_number] += 1
+
     def transition(self, timestamp):
         arrival = random.uniform(0.00000, 1.00000)
-        for idx, prob in enumerate(self.probs):
+        for idx, prob in enumerate(self.probabilities.probs):
             if arrival <= prob['prob']:
                 if prob['name'] == 'staying_prob':
                     self.no_processing_request_number += 1
-                self.change_state[prob['name']]()
-                self.occurrences_per_time.setdefault(timestamp, 0)
-                self.occurrences_per_time[timestamp] += self.request_number
-                self.occurrences_per_state.setdefault(self.request_number, 0)
-                self.occurrences_per_state[self.request_number] += 1
+                self.probabilities = self.probabilities.next(prob['name'])
+                self.log_stats(timestamp)
                 return True
         return None
 
 
 server = Server()
-print(f"probs: {server.probs}")
 for i in range(100000):
     server.transition((i + 1) * 10)
 server.graph_occurrences_per_time()
